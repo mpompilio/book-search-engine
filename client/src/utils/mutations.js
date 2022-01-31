a72f52e21ac886f261d3bc81e0859458a8b738e6
Matthew Pompilio <mattflynnpomp@aol.com>
2022-01-31 17:39:30 -0500

    adding def and resolver

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,40 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_BOOK = gql`
+  mutation addBook($title: String!){
+      addBook(title: $title) {
+          _id
+          bookId
+          authors
+          description
+          title
+          image
+          link
+      }
+  }
+
+`;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,44 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+
+type Book {
+    _id: ID
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+type Auth {
+    token: ID!
+    user: User
+  }
+
+type Query {
+    me: User
+}
+  
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(book: SavedBookInput): User
+    removeBook(bookId: String!): User
+}
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
